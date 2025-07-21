c8e9557979e4c8c353c4c801faf78824e73a36c1
hdyoon <skymegod@gmail.com>
2025-07-21 10:44:20 +0900

    first commit

---
+++ src/main/java/com/example/Hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.Hello.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<?> returnHello(){
+        return ResponseEntity.ok("Hello You");
+    }
+}
